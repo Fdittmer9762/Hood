3b2ec770ccb75671b79c00cb835c8a37d3beab1c
Fritz Dittmer <fdittmer9762@gmail.com>
2016-07-27 12:02:04 -0600

    Obstacle Scripting
    resolving issues with Obstacle script; issues refrencing other object tags

--- Hood/Assets/Scripts/ObstController.cs
+++ Hood/Assets/Scripts/ObstController.cs
@@ -5,7 +5,12 @@ public class ObstController : MonoBehaviour {
 
 	public int explosionTime;
 
-	void OnColliderEnter (Collider other) {
+	IEnumerator TimeDelay (int time) {
+		yield return new WaitForSeconds(time);
+	}
+	//had to take it from the delay script, wasn't able to implament it from there, try to fix later
+
+	void OnTriggerEnter (GameObject other) {
 		/*if (other.tag == Player) {
 			//set player state to death
 			RedStates.currentRedState = RedStates.redState.Death;
@@ -17,19 +22,10 @@ public class ObstController : MonoBehaviour {
 			//set state to explode
 			ObstState.currentObstacleState = ObstState.obstacleState.exploding;
 			//let animation run, try to use coroutine
-			Delay.TimeDelay(explosionTime);
+			TimeDelay (explosionTime);
 			//set state to destroyed
 			ObstState.currentObstacleState = ObstState.obstacleState.destroyed;
-		}
-		if (other.tag == Boundary) {
-			//destroy itself, or set to inactive. whatever is more efficent. inactive may cause mem leak
 		}*/
 	}
-
-	/*void OncolliderExit (Collider other) {
-		//when the boandary is exited destroy the object
-	}*/
-
-	//   **deciding on the best way to handle it, keeping code just in case**
-	//   **may consider replacing if statements with an array and switch statement, will try later**
+		//**deleted** handled by boundary script
 }
