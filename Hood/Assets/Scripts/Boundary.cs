198707323a71b8038e9dc23e1dc236034e7296ad
Fritz Dittmer <fdittmer9762@gmail.com>
2016-07-27 11:16:07 -0600

    Creating Boundarys
    scripting and objects to remove objects leaving the game area (not working)

---
+++ Hood/Assets/Scripts/Boundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+
+	void OnColliderEnter (Collider other){
+		//destroys objects that leave the boundarys
+		DestroyObject (other.gameObject);
+	}
+
+}
