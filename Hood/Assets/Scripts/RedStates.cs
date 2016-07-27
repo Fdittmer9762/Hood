5a8720cf706d4bc483e47faedc298a660dd06494
Fritz Dittmer <fdittmer9762@gmail.com>
2016-07-27 12:28:22 -0600

    Creating Red
    created red prefab, animations and started animator controller, adding animation script and triggers next

--- Hood/Assets/Scripts/RedStates.cs
+++ Hood/Assets/Scripts/RedStates.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class RedStates : MonoBehaviour {
 
-	public enum redState {Running, Jumping, Falling, Landing, Attacking, Death};
+	public enum redState {Running, Jumping, Falling, Landing, Attacking, Death}; //still need anim for death
 
 	public static redState currentRedState = redState.Running;
 
