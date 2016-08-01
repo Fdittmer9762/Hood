ed2e87643df4187b5ab4c0ae775daf948cb3e4b8
Fritz Dittmer <fdittmer9762@gmail.com>
2016-08-01 13:48:57 -0600

    Red Controller Psudo
    finished psudo code for red

--- Hood/Assets/Scripts/RedController.cs
+++ Hood/Assets/Scripts/RedController.cs
@@ -4,31 +4,36 @@ using System.Collections;
 public class RedController : MonoBehaviour {
 
 	public int health;
-	
-	// Update is called once per frame
+
 	void Update () {
 		switch (RedStates.currentRedState) {
+		case RedStates.redState.Running:
+			//default state, play the running animation
+			break;
 		case RedStates.redState.Jumping:
-			//
+			//while the state is jump (player is holding jump) move the player up
+			//when the height reaches the limit switch to falling
 			break;
 		case RedStates.redState.Falling:
-			//
+			//fall unitl player reaches the ground,
 			break;
 		case RedStates.redState.Landing:
-			//
+			//stop player from falling, set redstate to landing, 
 			break;
 		case RedStates.redState.Attacking:
-			//
+			//attack coroutine, using coroutine due to waitfor and other benifits
+			//attack coruotine: start attack animation, wait for x time, spawn fire bolt, 
+			//after coroutine set animation 
 			break;
 		case RedStates.redState.Damage:
+			//decrement health, check health, if health is gone kill player
 			if (health < 0) {
-				print ("Health: " + health);
+				health--;
+				print ("Health: " + health); //to check functionallity, will replace with gui
+				RedStates.currentRedState = RedStates.redState.Running;
 			} else {
 				RedStates.currentRedState = RedStates.redState.Death;
-				GameStates.currentGameState = GameStates.gameState.GameOver;
-				//this.gameObject.SetActive (false);  killing the player, will use different method
 			}
-			RedStates.currentRedState = RedStates.redState.Running;
 			break;
 		case RedStates.redState.Death:
 			GameStates.currentGameState = GameStates.gameState.GameOver;
