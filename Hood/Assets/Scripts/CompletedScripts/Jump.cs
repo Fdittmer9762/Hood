d23f6c507b547f397f7d7030a54266ca1a480f97
Fritz Dittmer <fdittmer9762@gmail.com>
2016-08-06 11:49:32 -0600

    Jumping Added
    Jumping working, working on animation bugs, will resolve later

--- Hood/Assets/Scripts/CompletedScripts/Jump.cs
+++ Hood/Assets/Scripts/CompletedScripts/Jump.cs
@@ -5,14 +5,19 @@ public class Jump : MonoBehaviour {
 
 	public Rigidbody redRB;
 	public Vector3 force;
+	public Animator redAnim;
 
 	void Start (){
 		redRB = GetComponent<Rigidbody> ();
+		redAnim = GetComponent<Animator> ();
 	}
 
 	void Update () {
+	//void OnMouseDown (){
 		if (Input.GetKeyDown ("space")) {
 			redRB.velocity = force;
+			redAnim.SetTrigger("Jumping");//play jumping animation
+		Debug.Log ("jump"); //debug
 		}
 	}
 }
---
+++ Hood/Assets/Scripts/Grounded.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grounded : MonoBehaviour {
+
+	public Animator redAnim;
+
+	void Start (){
+		redAnim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter() {
+		redAnim.SetTrigger ("Grounded");
+		Debug.Log ("landing");
+	}
+}
