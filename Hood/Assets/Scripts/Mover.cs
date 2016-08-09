46d825293c5997d59c8481e45d11e7127eab076e
Fritz Dittmer <fdittmer9762@gmail.com>
2016-08-09 17:23:50 -0600

    Ground Prefabs
    created ground prefabs, no ironed out any issues, all working

---
+++ Hood/Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public Vector3 score;
+	public Rigidbody rb;
+
+	void Update (){
+		rb = GetComponent <Rigidbody> ();
+		rb.velocity = score;
+	}
+}
