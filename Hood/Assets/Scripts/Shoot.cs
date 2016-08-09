0c08234b5dee7c565c082a87cc3b06f8d2bb8c53
Fritz Dittmer <fdittmer9762@gmail.com>
2016-08-09 16:08:58 -0600

    Player "Red" Completed
    Red is shooting, jumping, and dying like she needs to. (needs death anim)

--- Hood/Assets/Scripts/Shoot.cs
+++ Hood/Assets/Scripts/Shoot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour {
 
-	public float waitTime =.1f;
+	public float waitTime =.5f;
 	public GameObject bolt;
 	public Transform boltSpawn;
 	public Animator redAnim;
@@ -12,21 +12,15 @@ public class Shoot : MonoBehaviour {
 		redAnim = GetComponent<Animator>();
 	}
 
-	void Fire(){ //use for testing until animation is added
-		redAnim.SetTrigger ("Attacking");
-		Debug.Log ("fire!");//debugging
-		Instantiate(bolt, boltSpawn.position, Quaternion.identity);
-	}
-
 	IEnumerator Attack () {
 		redAnim.SetTrigger ("Attacking");//call attack animation,
 		yield return new WaitForSeconds(waitTime); 	//wait for time allows animation to align
-		Debug.Log ("fire!");//debugging
+		//Debug.Log ("fire!");//debugging
 		Instantiate(bolt, boltSpawn.position, Quaternion.identity); //spawn firebolt
 	}
 
 	void OnMouseDown(){//can be placed on a fire buttton or on the player
-		Fire(); //replace with coroutine later
+		StartCoroutine (Attack());//fires bolt and plays attack
 	}
 
 }
